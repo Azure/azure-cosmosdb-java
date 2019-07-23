f3bbe5a4796fcd4513a5335fa420ae73b86e6f77
milismsft-mac <adrianmi@microsoft.com>
2019-07-23 13:00:53 -0700

    Remove blocking calls from partition supervisor implementation.

--- sdk/src/main/java/com/azure/data/cosmos/internal/changefeed/implementation/PartitionSupervisorImpl.java
+++ sdk/src/main/java/com/azure/data/cosmos/internal/changefeed/implementation/PartitionSupervisorImpl.java
@@ -86,7 +86,8 @@ class PartitionSupervisorImpl implements PartitionSupervisor, Closeable {
             Thread processorThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    self.processor.run(self.processorCancellation.getToken()).block();
+                    self.processor.run(self.processorCancellation.getToken())
+                        .subscribe();
                 }
             });
 
@@ -95,7 +96,8 @@ class PartitionSupervisorImpl implements PartitionSupervisor, Closeable {
             Thread renewerThread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    self.renewer.run(self.renewerCancellation.getToken()).block();
+                    self.renewer.run(self.renewerCancellation.getToken())
+                        .subscribe();
                 }
             });
 
